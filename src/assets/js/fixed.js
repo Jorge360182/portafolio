31effe7be346e1a1de70fd131e8cccbe9b7cd554
Jorge360182 <z-5green-360@hotmail.com>
2023-08-24 09:12:22 -0600

    incluye modificacion nav

--- src/assets/js/fixed.js
+++ src/assets/js/fixed.js
@@ -1,34 +1,32 @@
 (function () {
-
     let navbar = document.querySelector('#navbar');
     let main = document.querySelector('main');
     let navbarHeight = navbar.getBoundingClientRect().height;
     let breakpoint = main.offsetTop - navbarHeight;
-    let windowPos;
+    let prevScrollPos = window.scrollY;
     let isFixed = false;
 
-    function updatePos() {
-        windowPos = window.scrollY;
-    }
-
     function onScroll() {
+        let currentScrollPos = window.scrollY;
 
-        updatePos();
-    
-        if (windowPos >= breakpoint && !isFixed) {
+        if (currentScrollPos >= breakpoint && !isFixed) {
             navbar.classList.remove('open');
-
             navbar.classList.add('navbar-fixed');
-            main.style.cssText = "margin-top: " + navbarHeight + 'px;';
             isFixed = true;
-    
-        } else if (windowPos < breakpoint && isFixed) {
+        } else if (currentScrollPos < breakpoint && isFixed) {
             navbar.classList.remove('navbar-fixed');
-            main.style.cssText = "margin-top: " + 0;
             isFixed = false;
         }
+
+        if (currentScrollPos > prevScrollPos && isFixed) {
+            navbar.classList.remove('open');
+            navbar.classList.add('navbar-hidden');
+        } else if (currentScrollPos < prevScrollPos && isFixed && currentScrollPos > breakpoint) {
+            navbar.classList.remove('navbar-hidden');
+        }
+
+        prevScrollPos = currentScrollPos;
     }
 
     document.addEventListener('scroll', onScroll);
-
-})()
+})();
